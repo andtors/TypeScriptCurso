ec2554e98d5f4897fe1afb9ade7d05519e051997
André Torres <andre.tors448@gmail.com>
2024-06-06 14:29:20 -0300

    SC-9-Concluido

---
+++ SC-9-Modules/greet.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function importGreet() {
+    console.log('Olá está função foi importada!');
+}
+exports.default = importGreet;
